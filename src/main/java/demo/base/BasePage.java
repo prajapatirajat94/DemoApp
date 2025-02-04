b46220947723b4df2689c8cedbbbb2a884c21530
Prajapatirajat94 <prajapatirajat94@gmail.com>
2025-02-03 23:48:15 -0500

    MacJenkins

--- src/main/java/demo/base/BasePage.java
+++ src/main/java/demo/base/BasePage.java
@@ -12,14 +12,10 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import demo.utils.OptionsManager;
-
 
 public class BasePage {
 	public WebDriver driver;
@@ -41,7 +37,7 @@ public class BasePage {
 		optionsmanager = new OptionsManager(prop);
 		String browserVersion = prop.getProperty("browserversion").trim();
 		if (browser.equalsIgnoreCase("chrome")) {
-			
+
 //Remote
 			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
 				init_remoteDriver("chrome");
@@ -51,7 +47,7 @@ public class BasePage {
 				// driver = new ChromeDriver();
 			}
 		} else if (browser.equalsIgnoreCase("firefox")) {
-			
+
 //Remote		
 			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
 				init_remoteDriver("firefox");
@@ -75,46 +71,49 @@ public class BasePage {
 	 */
 	private void init_remoteDriver(String browser) {
 		if (browser.equals("chrome")) {
-			
-			/* USE BELOW ONLY IF YOU ARE USING 3.*.* VERSION OF SELENIUM	 
-			// Selenium 3.x.x
-			// DesiredCapabilities cap = DesiredCapabilities.chrome();
 
-			// Selenium 4.x.x
-			/* in 4.x.x desiredcap is depricated so we are passing capabilities in chromoption
-			DesiredCapabilities cap = new DesiredCapabilities();
-			
-			 cap.setBrowserName("chrome");
-			/* THIS THING TO SET FOR SELENOID */
 			/*
-			cap.setCapability("browserName", "chrome");
-			cap.setCapability("browserVersion", browserVersion);
-			cap.setCapability("enableVNC", true);
-			cap.setCapability(ChromeOptions.CAPABILITY, optionsmanager.getChromeOptions());
-			*/
-			
+			 * USE BELOW ONLY IF YOU ARE USING 3.*.* VERSION OF SELENIUM // Selenium 3.x.x
+			 * // DesiredCapabilities cap = DesiredCapabilities.chrome();
+			 * 
+			 * // Selenium 4.x.x /* in 4.x.x desiredcap is depricated so we are passing
+			 * capabilities in chromoption DesiredCapabilities cap = new
+			 * DesiredCapabilities();
+			 * 
+			 * cap.setBrowserName("chrome"); /* THIS THING TO SET FOR SELENOID
+			 */
+			/*
+			 * cap.setCapability("browserName", "chrome");
+			 * cap.setCapability("browserVersion", browserVersion);
+			 * cap.setCapability("enableVNC", true);
+			 * cap.setCapability(ChromeOptions.CAPABILITY,
+			 * optionsmanager.getChromeOptions());
+			 */
+
 			try {
-				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsmanager.getChromeOptions()));
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsmanager.getChromeOptions()));
 			} catch (MalformedURLException e) {
 				e.printStackTrace();
 			}
-			
-			
+
 		} else if (browser.equals("firefox")) {
 //Selenium 3.x.x
 			// DesiredCapabilities cap = DesiredCapabilities.firefox();
 
 //Selenium 4.x.x
-		//	DesiredCapabilities cap = new DesiredCapabilities();
+			// DesiredCapabilities cap = new DesiredCapabilities();
 			// cap.setBrowserName("firefox");
 			/*
 			 * THIS THING TO SET FOR SELENOID cap.setCapability("browserName", "firefox");
 			 * cap.setCapability("browserVersion", "57.0"); cap.setCapability("enableVNC",
 			 * true);
 			 */
-			//cap.setCapability(FirefoxOptions.FIREFOX_OPTIONS, optionsmanager.getFirefoxOptions());
+			// cap.setCapability(FirefoxOptions.FIREFOX_OPTIONS,
+			// optionsmanager.getFirefoxOptions());
 			try {
-				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsmanager.getFirefoxOptions()));
+				tlDriver.set(
+						new RemoteWebDriver(new URL(prop.getProperty("huburl")), optionsmanager.getFirefoxOptions()));
 			} catch (MalformedURLException e) {
 
 				e.printStackTrace();
@@ -134,12 +133,12 @@ public class BasePage {
 	public Properties Base_properties() {
 		try {
 			prop = new Properties();
-			//for MacBook
-//			FileInputStream file = new FileInputStream("/Users/rajatkumarprajapati/eclipse-workspace/Demoapp/src"
-//					+ "/main/java/demo/config/config.properties");
+			// for MacBook
+			FileInputStream file = new FileInputStream("/Users/rajatkumarprajapati/eclipse-workspace/Demoapp/src"
+					+ "/main/java/demo/config/config.properties");
 //			System.out.println(System.getProperty("user.dir")+"\\src\\main\\java\\demo\\config\\config.properties");
-			FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\demo\\config\\config.properties");
-            
+//			FileInputStream file = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\demo\\config\\config.properties");
+
 			prop.load(file);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
@@ -153,11 +152,11 @@ public class BasePage {
 	 */
 	public String getScreenshot() {
 		File Srcfile = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
-		//for macbook
+		// for macbook
 //		String path = "/Users/rajatkumarprajapati/eclipse-workspace/Demoapp/screenshots/" + System.currentTimeMillis()
 //		+ ".png";
-		
-		String path =  System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
+
+		String path = System.getProperty("user.dir") + "/screenshots/" + System.currentTimeMillis() + ".png";
 		try {
 			FileUtils.copyFile(Srcfile, new File(path));
 		} catch (IOException e) {
